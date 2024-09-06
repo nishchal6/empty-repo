3da3a35b9385f7c97be32d56f46f28e219df058f
root <root@ip-172-31-68-37.ec2.internal>
2024-09-06 07:23:14 +0000

    second

--- scripts/restart_server.sh
+++ scripts/restart_server.sh
@@ -3,5 +3,6 @@
 # Navigate to the application directory
 cd /var/www/html/my-react-app
 
-# Restart the application (adjust according to your setup)
-pm2 restart my-react-app || npm start
+# Start the application
+npm start
+
